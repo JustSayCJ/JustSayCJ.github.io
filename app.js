c72e37ea2c7bde44ffc25c1bd3f2c34fb75ed5e4
JustSayCJ <XReateHD@gmail.com>
2020-01-14 21:54:52 +0100

    EC

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', init);
+
+function init() {
+    console.log('DOM is loaded');
+}
+
+function gradeColor() {
+    let grade = document.getElementsByClassName("cijfer");
+    
+}
